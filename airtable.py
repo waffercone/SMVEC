a956cea1a8b8d431db4f02c4a488cf1b5d70df5f
waffercone <149046452+waffercone@users.noreply.github.com>
2024-07-19 16:33:27 +0530

    Update airtable.py

--- airtable.py
+++ airtable.py
@@ -1,6 +1,6 @@
 import requests
-AIRTABLE_BASE_ID = "appjrOltyQk231hcd"
-AIRTABLE_API_KEY = "patMrEIyQnnfwMI1I.18eef26ab44daafc0538c0c0e4f107ee7295be52699eb666d2b61db0cb210c04"
+AIRTABLE_BASE_ID = "appB6LFUMhkx681r3"
+AIRTABLE_API_KEY = "patvOdvupRPT8kBRg.9caf5e416b2f998138cc403bbd375b52f8df131c2027e622da894813d5622403"
 AIRTABLE_TABLE_NAME = "chatbot"
 endpoint = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
 
@@ -8,11 +8,12 @@ headers = {
 "Authorization": f"Bearer {AIRTABLE_API_KEY}",
 "Content-Type": "application/json"
 }
-def save_rec(user_query,response):
+def save_rec(user_query,response,ip_address):
     data = {
   "records": [
           {
         "fields": {
+            "IP_address": f"{ip_address}",
             "Query": f"{user_query}",
             "Response": f"{response}"
         }
@@ -25,4 +26,4 @@ def save_rec(user_query,response):
 
 
 if __name__ == '__main__':
-    save_rec("hello","hi there")
+    save_rec("hello","hi there","ip")
