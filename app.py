2d777cbf505889e6f7b5ae8e06d3f83cb0c5f03e
waffercone <149046452+waffercone@users.noreply.github.com>
2024-07-19 16:33:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+
 from flask import Flask ,jsonify,render_template,request
 from flask_cors import CORS
-from chat import get_response
 from airtable import save_rec
+from assistants import get_response
+
 app = Flask( __name__) 
 CORS(app)
 #@app.get("/")
@@ -10,10 +12,12 @@ CORS(app)
 
 @app.post("/predict")
 def predict():
+    
     text = request.get_json().get("message")
+    ip = request.headers['X-Forwarded-For']
     response = get_response(text)
     message = {"answer": response}
-    save_rec(text, response)
+    save_rec(text,response,ip)
     return jsonify(message)
 
 
